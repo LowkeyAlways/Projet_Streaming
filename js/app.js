d7ccbc9851fc496ac7521e8eb0016a9f9d706e1c
AndyL94 <andy.louis94@outlook.fr>
2022-03-22 14:28:07 +0100

    JS changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log("Loaded")
-    let form, parents
-        // Chercher les éléments du dom
-        parents = document.querySelector("body")
-        console.log(parents.parentElement)
-        console.log(parents.childNodes.length+" éléments dans le document")
-        console.log(parents.childNodes[3].childNodes)
-    
-    
-    // form object
+document.addEventListener("DOMContentLoaded", e => {
+    let form
+
     form = ({
         name: document.querySelectorAll("input")[0],
         btn : document.querySelectorAll("input")[4],
@@ -16,11 +8,12 @@ document.addEventListener("DOMContentLoaded",()=>{
             this.name.value ? console.log(this.name.value) : console.log("remplir les champs")
         }
     })
-    
-    //event pour capter si les champs sont vides
-   form.btn.addEventListener("click",(e)=>{
+
+    form.btn.addEventListener("click",(e)=>{
         e.preventDefault()
         form.valider();
-        //console.log(e.target.tagName)  
+        
+        
     })
-})
+
+});
