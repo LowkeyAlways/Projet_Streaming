8f2b078aee0192f2daa326da63ecd8b4c61d0fac
AndyL94 <andy.louis94@outlook.fr>
2022-03-28 16:15:08 +0200

    Update CSS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", e => {
-    let form
+    let form;
 
-    form = ({
+    form = {
         name: document.querySelectorAll("input")[0],
-        btn : document.querySelectorAll("input")[4],
-        valider(){
-            this.name.value ? console.log(this.name.value) : console.log("remplir les champs")
+        btn: document.querySelectorAll("input")[4],
+        valider() {
+            this.name.value ? console.log(this.name.value) : console.log("remplir les champs");
         }
-    })
+    };
 
-    form.btn.addEventListener("click",(e)=>{
-        e.preventDefault()
+    form.btn.addEventListener("click", e => {
+        e.preventDefault();
         form.valider();
-        
-        
-    })
+    });
 
+    var email = document.getElementById("mail");
+
+    email.addEventListener("keyup", function (event) {
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity("J'attend un e-mail, mon cher&nbsp;!");
+        } else {
+            email.setCustomValidity("");
+        }
+    });
 });
