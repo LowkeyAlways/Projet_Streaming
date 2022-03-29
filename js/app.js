ff04623a5f741932bb53a2e7f1ad7249c63dec30
AndyL94 <andy.louis94@outlook.fr>
2022-03-29 14:54:52 +0200

    Update Modale

--- js/app.js
+++ js/app.js
@@ -1,5 +1,43 @@
-document.addEventListener("DOMContentLoaded", e => {
-    let form;
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log(navigator.userAgent);
+    let form, el, modal, closed, open_modal, closed_all, footer_right;
+
+    el = document.querySelectorAll(".image-box li");
+    modal = document.querySelector(".parent-modale");
+    closed = document.querySelector(".modale button");
+    closed_all = document.querySelector(".modale img");
+    footer_right = document.querySelector("footer p")
+     
+    /* property elements */
+  
+    open_modal = function () {
+      console.log(this.dataset);
+      /* les variables */
+      let image = this.dataset.image;
+      let title = this.dataset.title;
+      let desc = this.dataset.description;
+      let dates = this.dataset.dates;
+      modal.classList.add("modale-active"); /* ajouter la classe active */
+      /* sélectionner les sélecteurs html*/
+      document.querySelector(".modale img").setAttribute("src", image);
+      document.querySelector(".modale .desc h3").innerText = title;
+      document.querySelector(
+        ".modale .desc p"
+      ).innerHTML = `<strong>Déscription : </strong>${desc}`;
+      document.querySelector(".modale .desc time").innerText = `Annee: ${dates}`;
+      document
+        .querySelector(".modale .desc time")
+        .setAttribute("datetime", dates);
+    };
+    for (rows of el) {
+      rows.addEventListener("click", open_modal);
+    }
+    closed.addEventListener("click", () => {
+      modal.classList.remove("modale-active");
+    });
+    closed_all.addEventListener("click", () => {
+      modal.classList.remove("modale-active");
+    });
 
     form = {
         name: document.querySelectorAll("input")[0],
